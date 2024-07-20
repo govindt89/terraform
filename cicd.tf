22c035afccc0dfc8e3902a5e58b883cb219563d7
govind89 <govind.taralkar@blazeclan.com>
2024-07-20 11:27:41 +0530

    this is with tagging

--- cicd.tf
+++ cicd.tf
@@ -8,7 +8,10 @@ resource "aws_instance" "myec2" {
   ami                  = "ami-0ad21ae1d0696ad58"
   instance_type        = "t2.micro"
   iam_instance_profile = "ec2-ssm-role"
-  subnet_id            = "subnet-07771a56e7430fc95"
+  subnet_id            = "subnet-0011192cf0a9540cc"
+   tags = {
+    Name = "Myec2instance"
+  }
  
 
 }
